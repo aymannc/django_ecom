9a0afb8a334055f14f0f95445e355d993f337fc4
aymannc <ayman.nait@gmail.com>
2019-10-26 02:13:13 +0100

    code update 26/10

--- ecom/settings.py
+++ ecom/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'tinymce',
 ]
 SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
